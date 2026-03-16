4a38116a7a0b7f14c1ba72755a2d747c2e61ed07
Chambit Oh <chambit.oh@gmail.com>
2026-03-16 21:40:12 +0200

    Routing and Test

--- ktor-sample/src/main/kotlin/Application.kt
+++ ktor-sample/src/main/kotlin/Application.kt
@@ -1,9 +1,16 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(
+        Netty,
+        port = 9292,
+        host = "localhost",
+        module = Application::module
+    ).start(wait = true)
 }
 
 fun Application.module() {
